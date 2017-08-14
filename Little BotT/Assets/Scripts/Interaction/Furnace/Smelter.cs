321f093498fe2237be0a45df52799991aa19dd9a
DokZokiu <dokzokiu@gmail.com>
2017-08-14 21:45:22 +0200

    Furnace WIP 1

--- Little BotT/Assets/Scripts/Interaction/Furnace/Smelter.cs
+++ Little BotT/Assets/Scripts/Interaction/Furnace/Smelter.cs
@@ -1,23 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Smelter : MonoBehaviour {
 
 
-	#region Variables
+    #region Variables
+    public CoalSlotFurnace CoalSlot;
+    public InputSlotFurnace InputSlot;
+    public Slider Slider;
+    public bool isWorking = false;
     
 	#endregion
 
 	#region Unity Methods
 
 	void Start () {
-		
+        CoalSlot = GetComponentInChildren<CoalSlotFurnace>();
+        InputSlot = GetComponentInChildren<InputSlotFurnace>();
+        
 	}
 	
 	
 	void Update () {
-		
+        if (!isWorking)
+        {
+            if (InputSlot.GetComponent<Slots>().numberObjects > 0 && CoalSlot.GetComponent<CoalSlotFurnace>().CoalBar.value > 10)
+            {
+                isWorking = true;
+                CoalSlot.GetComponent<CoalSlotFurnace>().CoalBar.value -= 10;
+                BlockInfoClass SmeltedBlock = InputSlot.GetComponent<Slots>().StockedObject;
+                InputSlot.GetComponent<Slots>().numberObjects -= 1;
+
+                
+
+            }
+        }
 	}
 
 	#endregion
